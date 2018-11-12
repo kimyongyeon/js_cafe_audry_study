4bc47a61575d2edd2b815ea605b3d7ad520572bf
eves <evesjapan@gmail.com>
2018-11-12 11:52:51 +0900

    source update

--- src/es6_ch_03.js
+++ src/es6_ch_03.js
@@ -104,4 +104,149 @@ export class Chapter3 {
         weakSet.add(obj)
         console.log(weakSet.has(obj))
     }
+
+    fnSymbol() {
+        // 문제 만들기
+        let arr = [1,2,3]
+        console.log(arr.length)
+        arr.length = 100 // 내장 속성을 덮어 씌워졌다.
+        console.log(arr.length)
+
+        // 문제점 해결
+        const length = Symbol('length'); // 심볼선언
+        arr[length] = 100;
+        console.log(arr[length]) // 결과는 100
+        console.log(arr.length) // 결과는 3, 내장 속성이 그대로다 
+        
+        // 문제점 제시 
+        arr.prop = 100
+        for (let i in arr ) {
+            console.log(i) // 결과는 0,1,2,prop
+        }
+
+        // 문제 해결
+        let = prop = Symbol('prop')
+        arr[prop] = 100
+        for (let i in arr) {
+            console.log(i)
+        }
+
+    }
+
+    fnPromise() {
+        var cnt = 0
+        setTimeout(() => {
+            cnt++; // cnt 1증가
+        }, 1000)
+
+        console.log(cnt) // 결과는 0 (지연 함수안의 구문보다 먼저 실행됨)
+
+        // 지연 함수의 결과를 전달받기 위해 가독성이 떨어짐
+        var cnt = 0
+        setTimeout(()=> {
+            receiveCount(cnt++) 
+        }, 1000)
+        var receiveCount = ($cnt) => {
+            cnt = $cnt;
+            console.log(cnt) // 결과는 1
+        }
+        // promise 
+        let cnt = 0
+        let promise = new Promise((res, rej) => {
+            setTimeout(()=> {
+                cnt++
+                res(cnt)
+            })
+        })
+        promise.then(($cnt) => {
+            cnt = $cnt
+            console.log(cnt)
+        })
+
+        // 성공
+        let promise2 = new Promise((r1, r2) => {
+            setTimeout(()=> {
+                r1('success');
+
+            }, 1000)
+        })
+        promise2.then((val)=> {
+            console.log(val)
+        })
+
+        // 거절
+        let promise3 = new Promise((r1, r2) => {
+            setTimeout(()=>{
+                r2('Failed')
+            }, 1000)
+        })
+        promise3.then((val)=> console.log(val)) // 거절이 되었으므로 호출되지 않음.
+        promise3.catch((reason) => console.log(reason)) // 이행 거절시 catch 메서드 인수를 통해 거절 이유를 전달받음.
+
+        // Promise.all(iterable)
+        // 한꺼번에 여러 promise 생성자의 이행 결과를 모아 전달
+        let p1 = new Promise((r1, r2) => {
+            setTimeout(() => {
+                r1('p1 fulfilled')
+            }, 2000) // 2초 뒤 실행
+        })
+        let p2 = new Promise((r1, r2) => {
+            setTimeout(() => {
+                r1('p2 fulfilled')
+            }, 1000) // 2초 뒤 실행
+        })
+        let iterable = [p1, p2]
+        Promise.all(iterable).then((value)=> { // Promise.all 메서드를 호출하고, 인자 iterable을 전달
+            console.log(value) // 결과는 ['p1 fulfilled', 'p2 fulfilled']
+        }, (reason) => { // 모두 이행이므로 호출되지 않음.
+            console.log(reason)
+        })
+        // Promise.race(iterable) 메서드는 여러 Promise 생성자를 경합하여 가장 빠른 Promise 생성자의 이행 결과를 전달합니다.
+        let p3 = new Promise((r1, r2) => {
+            setTimeout(() => {
+                r1('p3 fulfilled')
+            }, 2000) // 2초 뒤 실행
+        })
+        let p4 = new Promise((r1, r2) => {
+            setTimeout(() => {
+                r1('p4 fulfilled')
+            }, 1000) // 2초 뒤 실행
+        })
+        let iterable = [p3, p4]
+        Promise.race(iterable).then((value)=> { // Promise.all 메서드를 호출하고, 인자 iterable을 전달
+            console.log(value) // 결과는 'p2 fulfilled' 
+        }, (reason) => { // 모두 이행이므로 호출되지 않음.
+            console.log(reason)
+        })
+    }
+
+    fnProxy() {
+        // let proxy = new Proxy(target, handler);
+        let target = {}
+        let handler = {
+            set: (target, key, value, receiver) => {
+                console.log(key, value)
+                return Reflect.set(target, key, value, receiver)
+            }
+        }
+        let proxy = new Proxy(target, handler)
+        proxy.name = 'my proxy'
+        console.log(target)
+        let foo = () => {}
+        foo.prototype.a = 100
+        foo.prototype.b = 200
+        let obj = new foo() // new 연산자를 수행하여 프로토타입 속성을 참조한 객체를 생성 
+        let proxy = new Proxy(obj, { // Proxy 객체를 생성하고 타켓 객체를 참조 
+            getPrototypeOf: (target) => { // 트랩설정 
+                console.log('트랩호출')
+                return Reflect.getPrototypeOf(target) // 타켓의 프로토타입 속성을 반환 
+            }
+        })
+        let proto = proxy.__proto__; // 객체의 프로토타입 속성을 얻기 
+        console.log(proto) // 결과는 객체의 프로토타입(a:100, b:200)
+        // 결과: 
+        // 트랩호출
+        // {a: 100, b:200}
+        
+    }
 }
